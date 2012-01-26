7e0faa0c278002f94b6a739371cbe7c2e83b8b13
rousseau <rousseau@unistra.fr>
2012-01-26 11:35:53 +0100

    Add the possibility of LR image simulation in the iterative backprojection algorithm as an option.

--- Utilities/btkIteratedBackProjection.cxx
+++ Utilities/btkIteratedBackProjection.cxx
@@ -102,6 +102,7 @@ int main( int argc, char *argv[] )
     TCLAP::MultiArg<std::string> transArg("t","transform","transform file",false,"string",cmd);
     TCLAP::ValueArg<int> loopArg  ("l","loop","Number of loops (iterations of IBP algorithm).", false,1,"int",cmd);
     TCLAP::ValueArg<int> nlmArg  ("n","nlm","Type of filtering during IBP process (0: no filtering, 1: error map filtering, 2: current HR image filtering).", false,0,"int",cmd);
+    TCLAP::ValueArg<int> simArg  ("s","sim","Simulation of LR images based on the input HR image (0: no simulation, 1: simulation).", false,0,"int",cmd);
     
     // Parse the argv array.
     cmd.parse( argc, argv );
@@ -115,6 +116,7 @@ int main( int argc, char *argv[] )
     std::string output_file      = outArg.getValue();
     int loops                    = loopArg.getValue();
     int nlm                      = nlmArg.getValue();
+    int simulation               = simArg.getValue();
     
     // typedefs
     const   unsigned int    Dimension = 3;
@@ -160,8 +162,10 @@ int main( int argc, char *argv[] )
     
     btkSRM.Initialize();
     
-    //btkSRM.SimulateLRImages();
-    //btkSRM.data.WriteSimulatedLRImages(input_file);
+    if(simulation==1){
+      btkSRM.SimulateLRImages();
+      btkSRM.data.WriteSimulatedLRImages(input_file);
+    }
     
     btkSRM.IteratedBackProjection(loops,nlm);
     btkSRM.data.WriteOutputHRImage(output_file);
