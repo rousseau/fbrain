d86a04df3e337d401667061de0bb609f7a87f14f
Marc Schweitzer <marc.schweitzer@unistra.fr>
2012-03-23 09:26:48 +0100

    Added skeleton classes for SuperResolution

---
+++ Code/Reconstruction/SuperResolution/btkHighResolutionReconstructionFilter.cxx
@@ -0,0 +1,21 @@
+#include "btkHighResolutionReconstructionFilter.h"
+
+namespace btk
+{
+
+HighResolutionReconstructionFilter::HighResolutionReconstructionFilter()
+{
+    btkCoutMacro(HighResolutionReconstructionFilter : Constructor );
+}
+
+HighResolutionReconstructionFilter::~HighResolutionReconstructionFilter()
+{
+    btkCoutMacro(HighResolutionReconstructionFilter : Destructor );
+}
+
+void HighResolutionReconstructionFilter::Update()
+{
+    btkCoutMacro(HighResolutionReconstructionFilter : Update Method );
+}
+
+}
