90b962aa36406b59cc04780d40e91e99710143d2
julienpontabry <pontabry@unistra.fr>
2012-07-20 11:21:10 +0200

    Add diffusion modelization.

---
+++ Code/Diffusion/btkSphericalHarmonicsDiffusionDecomposition.h
@@ -0,0 +1,68 @@
+/*==========================================================================
+  
+  © Université de Strasbourg - Centre National de la Recherche Scientifique
+  
+  Date: 12/07/2012
+  Author(s): Julien Pontabry (pontabry@unistra.fr)
+  
+  This software is governed by the CeCILL-B license under French law and
+  abiding by the rules of distribution of free software.  You can  use,
+  modify and/ or redistribute the software under the terms of the CeCILL-B
+  license as circulated by CEA, CNRS and INRIA at the following URL
+  "http://www.cecill.info".
+  
+  As a counterpart to the access to the source code and  rights to copy,
+  modify and redistribute granted by the license, users are provided only
+  with a limited warranty  and the software's author,  the holder of the
+  economic rights,  and the successive licensors  have only  limited
+  liability.
+  
+  In this respect, the user's attention is drawn to the risks associated
+  with loading,  using,  modifying and/or developing or reproducing the
+  software by the user in light of its specific status of free software,
+  that may mean  that it is complicated to manipulate,  and  that  also
+  therefore means  that it is reserved for developers  and  experienced
+  professionals having in-depth computer knowledge. Users are therefore
+  encouraged to load and test the software's suitability as regards their
+  requirements in conditions enabling the security of their systems and/or
+  data to be ensured and,  more generally, to use and operate it in the
+  same conditions as regards security.
+  
+  The fact that you are presently reading this means that you have had
+  knowledge of the CeCILL-B license and that you accept its terms.
+  
+==========================================================================*/
+
+#ifndef BTK_SPHERICAL_HARMONICS_DIFFUSION_DECOMPOSITION_H
+#define BTK_SPHERICAL_HARMONICS_DIFFUSION_DECOMPOSITION_H
+
+// ITK includes
+#include "itkVariableLengthVector.h"
+
+// Local includes
+#include "btkDiffusionModel.h"
+
+namespace btk
+{
+
+class SphericalHarmonicsDiffusionDecomposition : public itk::VariableLengthVector< float >, public btk::DiffusionModel
+{
+    public:
+        typedef SphericalHarmonicsDiffusionDecomposition Self;
+        typedef itk::VariableLengthVector< float > Superclass;
+
+        /**
+         * @brief Construct a SH coefficients container with no memory space allocated yet.
+         */
+        SphericalHarmonicsDiffusionDecomposition();
+
+        /**
+         * @brief Constructor.
+         * @param numberOfCoefficients Number of spherical harmonics coefficients.
+         */
+        SphericalHarmonicsDiffusionDecomposition(unsigned int numberOfCoefficients);
+};
+
+} // namespace btk
+
+#endif // BTK_SPHERICAL_HARMONICS_DIFFUSION_DECOMPOSITION_H
