bcfa99ef6a76a81a3fc2592e1477fbcc97a4f9b5
julienpontabry <pontabry@unistra.fr>
2013-01-04 10:38:21 +0100

    Add a utility for coloring fiber tracts by their mean orientation.

---
+++ Utilities/btkColorFiberTractsByMeanOrientation.cxx
@@ -0,0 +1,116 @@
+/*==========================================================================
+  
+  © Université de Strasbourg - Centre National de la Recherche Scientifique
+  
+  Date: 04/01/2013
+  Author(s): Julien Pontabry (pontabry@unistra.fr)
+  
+  This software is governed by the CeCILL-B license under French law and
+  abiding by the rules of distribution of free software.  You can  use,
+  modify and/ or redistribute the software under the terms of the CeCILL-B
+  license as circulated by CEA, CNRS and INRIA at the following URL
+  "http://www.cecill.info".
+  
+  As a counterpart to the access to the source code and  rights to copy,
+  modify and redistribute granted by the license, users are provided only
+  with a limited warranty  and the software's author,  the holder of the
+  economic rights,  and the successive licensors  have only  limited
+  liability.
+  
+  In this respect, the user's attention is drawn to the risks associated
+  with loading,  using,  modifying and/or developing or reproducing the
+  software by the user in light of its specific status of free software,
+  that may mean  that it is complicated to manipulate,  and  that  also
+  therefore means  that it is reserved for developers  and  experienced
+  professionals having in-depth computer knowledge. Users are therefore
+  encouraged to load and test the software's suitability as regards their
+  requirements in conditions enabling the security of their systems and/or
+  data to be ensured and,  more generally, to use and operate it in the
+  same conditions as regards security.
+  
+  The fact that you are presently reading this means that you have had
+  knowledge of the CeCILL-B license and that you accept its terms.
+  
+==========================================================================*/
+
+
+// TCLAP includes
+#include "tclap/CmdLine.h"
+
+// STL includes
+#include "cstdlib"
+#include "string"
+#include "iostream"
+
+// ITK includes
+#include "itkMacro.h"
+
+// VTK includes
+#include "vtkSmartPointer.h"
+#include "vtkPolyData.h"
+#include "vtkPolyDataReader.h"
+#include "vtkPolyDataWriter.h"
+
+// Local includes
+#include "btkPolyDataColorLinesByOrientation.h"
+
+
+int main(int argc, char *argv[])
+{
+    try
+    {
+        //
+        // Command line parser
+        //
+
+        // Command line definition
+        TCLAP::CmdLine cmd("btkColorFiberTractsByMeanOrientation: color the fibers (polydata) by their mean orientation", ' ', "1.0", true);
+
+        // Arguments
+        TCLAP::ValueArg< std::string >  inputFileNameArg("i", "input", "Input polydata filename (VTK)", true, "string", "", cmd);
+        TCLAP::ValueArg< std::string > outputFileNameArg("o", "output", "Output polydata filename (VTK)", true, "string", "", cmd);
+
+        // Parse the args.
+        cmd.parse(argc, argv);
+
+        // Get arguments' values
+        std::string  inputFileName = inputFileNameArg.getValue();
+        std::string outputFileName = outputFileNameArg.getValue();
+
+
+        //
+        // Processing
+        //
+
+        // Load fiber bundle
+        vtkSmartPointer< vtkPolyDataReader > reader = vtkSmartPointer< vtkPolyDataReader >::New();
+        reader->SetFileName(inputFileName.c_str());
+
+        // Process
+        vtkSmartPointer< btk::PolyDataColorLinesByOrientation > filter = vtkSmartPointer< btk::PolyDataColorLinesByOrientation >::New();
+        filter->SetInputConnection(reader->GetOutputPort());
+        // Write filtered fiber bundle
+        vtkSmartPointer< vtkPolyDataWriter > writer = vtkSmartPointer< vtkPolyDataWriter >::New();
+        writer->SetFileName(outputFileName.c_str());
+        writer->SetInputConnection(filter->GetOutputPort());
+        writer->Update();
+    }
+    catch(TCLAP::ArgException &e)
+    {
+        std::cerr << "Error: " << std::endl << e.error() << " for arg " << e.argId() << std::endl;
+        return EXIT_FAILURE;
+    }
+    catch(std::string &e)
+    {
+        std::cerr << "Error: " << std::endl << e << std::endl;
+        return EXIT_FAILURE;
+    }
+    catch(itk::ExceptionObject &e)
+    {
+        std::cerr << "Error: " << std::endl << e.GetDescription() << std::endl;
+        return EXIT_FAILURE;
+    }
+
+
+    return EXIT_SUCCESS;
+}
