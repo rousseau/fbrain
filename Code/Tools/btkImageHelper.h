885252a397e177366aca22543dd1ef94e6c4ebc9
Marc Schweitzer <marc.schweitzer@unistra.fr>
2013-01-24 18:08:38 +0100

    Add a const version of CreateNewImageFromPhysicalSpaceOf -> CreateNewImageFromPhysicalSpaceOfConst

--- Code/Tools/btkImageHelper.h
+++ Code/Tools/btkImageHelper.h
@@ -115,7 +115,23 @@ namespace btk
              * @param defaultValue Default value of pixel in new image.
              * @return New image in the same physical space.
              */
-            static typename TImageOutput::Pointer CreateNewImageFromPhysicalSpaceOf(typename TImageInput::ConstPointer image, typename TImageOutput::PixelType defaultValue=itk::NumericTraits< typename TImageOutput::PixelType >::ZeroValue());
+            static typename TImageOutput::Pointer CreateNewImageFromPhysicalSpaceOf(typename TImageInput::Pointer image, typename TImageOutput::PixelType defaultValue=itk::NumericTraits< typename TImageOutput::PixelType >::ZeroValue());
+
+            /**
+             * @brief Create a new image in the same physical space of a current image.
+             * @param image Image of which physical space will be used for creation.
+             * @param defaultValue Default value of pixel in new image.
+             * @return New image in the same physical space.
+             */
+            static typename TImageOutput::Pointer CreateNewImageFromPhysicalSpaceOfConst(typename TImageInput::ConstPointer image, typename TImageOutput::PixelType defaultValue=itk::NumericTraits< typename TImageOutput::PixelType >::ZeroValue());
+
+            /**
+             * @brief Create new images in the same physical space of current images.
+             * @param images Vector of images of which physical space will be used for creation.
+             * @param defaultValue Default value of pixel in new image.
+             * @return Vector of new images in the same physical space.
+             */
+            static std::vector< typename TImageOutput::Pointer > &CreateNewImageFromPhysicalSpaceOfConst(std::vector< typename TImageInput::ConstPointer > &images, typename TImageOutput::PixelType defaultValue=itk::NumericTraits< typename TImageOutput::PixelType >::ZeroValue());
 
             /**
              * @brief Create new images in the same physical space of current images.
