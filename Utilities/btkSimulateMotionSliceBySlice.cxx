641c88cf87b8dcc2457c459319c8ef36f9fb12d9
Marc Schweitzer <marc.schweitzer@unistra.fr>
2013-03-19 14:23:16 +0100

    Update slice by slice motion simulation

--- Utilities/btkSimulateMotionSliceBySlice.cxx
+++ Utilities/btkSimulateMotionSliceBySlice.cxx
@@ -38,12 +38,18 @@
 #include "btkEulerSliceBySliceTransform.h"
 #include "btkImageHelper.h"
 #include "btkIOTransformHelper.h"
+#include "btkApplyTransformToImageFilter.h"
+#include "btkMathFunctions.h"
 
 /* ITK */
 #include "itkImage.h"
 #include "itkResampleImageFilter.h"
 #include "itkAffineTransform.h"
 #include "itkEuler3DTransform.h"
+#include "itkLinearInterpolateImageFunction.h"
+#include "itkImageRegionIteratorWithIndex.h"
+#include "itkContinuousIndex.h"
+
 
 /* Others */
 #include "iostream"
@@ -60,29 +66,40 @@ int main(int argc, char* argv[])
     // TCLAP :
     TCLAP::CmdLine cmd("SimulateMotionSliceBySlice, Apply transformations on slices of input image.", ' ', "Unversioned");
     TCLAP::ValueArg<std::string> inputArg("i","input","input image.",true,"","string",cmd);
+    TCLAP::ValueArg<std::string> refArg("r","reference","reference image.",true,"","string",cmd);
     TCLAP::ValueArg<std::string> outputArg("o","output","output image.",true,"","string",cmd);
     TCLAP::ValueArg<std::string> transArg("t","transform","output transformation.",true,"","string",cmd);
-    TCLAP::ValueArg<int> motionLevelArg("l","level","level of motion. (1,2 or 3 the greater)",false,1,"int",cmd);
-    TCLAP::ValueArg<double> rotationLevelArg("","rotMax","Rotation max in radian.",false,0.2,"int",cmd);
-    TCLAP::ValueArg<double> translationLevelArg("","traMax","Translation max in mm.",false,1.5,"int",cmd);
+    TCLAP::ValueArg<int> motionLevelArg("l","level","level of motion. (1,2,3 or 4 for all slices)",false,2,"int",cmd);
+    TCLAP::ValueArg<double> rotationLevelArg("","rotMax","Rotation max in degree.",false,5,"int",cmd);
+    TCLAP::ValueArg<double> translationLevelArg("","traMax","Translation max in mm.",false,5,"int",cmd);
 
 
-    typedef itk::ResampleImageFilter< itkImage, itkImage > Resampler;
     typedef btk::EulerSliceBySliceTransform<double,3>      RigidSliceBySliceTransformation;
     typedef itk::Euler3DTransform<double>                  RigidTransformation;
+    typedef itk::ImageRegionIteratorWithIndex<itkImage> Iterator;
+    typedef itk::LinearInterpolateImageFunction<itkImage> Interpolator;
+    typedef itk::ContinuousIndex<itkImage> ContinuousIndex;
+    typedef btk::ApplyTransformToImageFilter<itkImage, itkImage > Resampler;
 
     cmd.parse(argc,argv);
 
     std::string inputFileName;
     std::string outputFileName;
     std::string transformFileName;
+    std::string refFileName;
 
     inputFileName = inputArg.getValue();
     outputFileName = outputArg.getValue();
     transformFileName = transArg.getValue();
+    refFileName = refArg.getValue();
     int level = motionLevelArg.getValue();
     double rotMax = rotationLevelArg.getValue();
     double traMax = translationLevelArg.getValue();
+
+     rotMax = btk::MathFunctions::DegreesToRadians(rotMax);
+
+     double rotMin =  -rotMax;
+     double traMin =  -traMax;
 
     int threshold = 0;
     switch(level)
@@ -98,6 +115,10 @@ int main(int argc, char* argv[])
         threshold = 3;
         break;
 
+    case 4:
+        threshold = 0;//all slice
+        break;
+
     default:
         threshold = 8;
         break;
@@ -108,7 +129,10 @@ int main(int argc, char* argv[])
 
     itkImage::Pointer inputImage = itkImage::New();
     itkImage::Pointer outputImage = itkImage::New();
+    itkImage::Pointer referenceImage = itkImage::New();
     inputImage = btk::ImageHelper<itkImage>::ReadImage(inputFileName);
+    referenceImage = btk::ImageHelper<itkImage>::ReadImage(refFileName);
+
 
     std::cout<<"The Program will compute a motion on several slices of input Image"<<std::endl;
 
@@ -127,8 +151,14 @@ int main(int argc, char* argv[])
 
     int NSlices = globalTransform->GetNumberOfSlices();
 
+    RigidTransformation::ParametersType randomParams(RandomTransform->GetNumberOfParameters());
+
+
     for(int i = 0; i<NSlices; i++)
     {
+        randomParams = globalTransform->GetSliceParameters(i);
+        globalTransform->SetSliceParameters(i, randomParams);
+
 
         std::cout<<"Slice N° : "<<i<<std::endl;
         int rndm = rand()%10;
@@ -144,22 +174,24 @@ int main(int argc, char* argv[])
             //Rotation
             if(rot_trans == 0)
             {
-                int paramR = rand()%2 ;
-                randomParams[paramR] = rand()/(double)RAND_MAX * rotMax;
+                int paramR = rand()%3 ; // x y and z
+                randomParams[paramR] = rand()/((double)RAND_MAX/std::abs(rotMax - rotMin )) - std::abs(rotMin);
             }
             //Translation
             else if(rot_trans == 1)
             {
-                int paramT = rand()%(2) + 3;
-                randomParams[paramT] = rand()/(double)RAND_MAX * traMax;
+                //if translation on z wanted change %2 in %3
+                int paramT = (rand()%2) + 3; // only x and y          
+                randomParams[paramT] = rand()/((double)RAND_MAX/ std::abs(traMax - traMin)) - std::abs(traMin);
             }
             //Rotation and translation
             else
             {
-                int paramR = rand()%2;
-                int paramT = rand()%2 + 3;
-                randomParams[paramR] = rand()/(double)RAND_MAX * rotMax;
-                randomParams[paramT] = rand()/(double)RAND_MAX * traMax;
+                int paramR = rand()%3;
+                //if translation on z wanted change %2 in %3
+                int paramT = (rand()%2) + 3;
+                randomParams[paramR] = rand()/((double)RAND_MAX/std::abs(rotMax - rotMin )) - std::abs(rotMin);
+                randomParams[paramT] = rand()/((double)RAND_MAX/ std::abs(traMax - traMin)) - std::abs(traMin);
             }
 
 
@@ -175,23 +207,30 @@ int main(int argc, char* argv[])
         {
             std::cout<<"Parameters "<<x<<" = "<<randomParams[x]<<std::endl;
             //Erase old parameters
-            randomParams[x] = 0.0;
+
         }
+        randomParams = globalTransform->GetSliceParameters(i);
 
     }
 
-    resampleFilter->SetOutputOrigin(inputImage->GetOrigin());
-    resampleFilter->SetOutputSpacing(inputImage->GetSpacing());
-    resampleFilter->SetOutputDirection(inputImage->GetDirection());
-    resampleFilter->SetTransform(globalTransform);
 
-    resampleFilter->SetInput(inputImage);
-    resampleFilter->SetSize(inputImage->GetLargestPossibleRegion().GetSize());
-    resampleFilter->Update();
+    Resampler::Pointer resampler = Resampler::New();
+
+    resampler->SetInputImage(inputImage);
+    resampler->SetReferenceImage(referenceImage);
+    resampler->SetTransform(globalTransform);
+
+    try
+    {
+        resampler->Update();
+    }
+    catch(itk::ExceptionObject &e)
+    {
+        std::cout<<e<<std::endl;
+    }
 
 
-    outputImage =  resampleFilter->GetOutput();
-    btk::ImageHelper< itkImage >::WriteImage(outputImage,outputFileName);
+    btk::ImageHelper< itkImage >::WriteImage(resampler->GetOutputImage(),outputFileName);
     btk::IOTransformHelper<RigidSliceBySliceTransformation>::WriteTransform(globalTransform,transformFileName);
 
     return EXIT_SUCCESS;
