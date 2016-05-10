60fa98635d9afea41f7490725b0854d263e85df7
rousseau <rousseau@salsa-27-223.priv.enst-bretagne.fr>
2016-05-10 09:22:03 +0200

    HR initialization is now required

--- pybtk/algorithms/3DSuperResolution.py
+++ pybtk/algorithms/3DSuperResolution.py
@@ -58,7 +58,7 @@ if __name__ == '__main__':
   parser.add_argument('-t', '--transform', help='Transform for each input image (optional)', action='append')
   parser.add_argument('-m', '--mask', help='Mask for each input image (optional)', action='append')
   parser.add_argument('-o', '--output', help='Estimated high-resolution image filename (required)', type=str, required = True)
-  parser.add_argument('--init', help='Image filename for initialization (optional)')
+  parser.add_argument('--init', help='Image filename for initialization (required)', type=str, required = True)
   parser.add_argument('-r', '--resolution', help='Resolution of the output HR image (1 for isotropic case, or 3 for anisotropic HR image). If not provided, the minimum size of the input image will be used.', action='append')
   parser.add_argument('-p', '--psf', help='3D PSF type (boxcar (default), gauss)', type=str, default='boxcar')
   parser.add_argument('--maxiter', help='Maximum number of iterations (SR optimization)', type=int, default=10)
@@ -150,52 +150,11 @@ if __name__ == '__main__':
   print 'Resolution for image reconstruction: '
   print HRSpacing  
   
-  ###---- Computing initialization if not provided ----------------------------
-  if args.init is not None:
-    initHRImage = nibabel.load( args.init )
-    data = np.zeros(initHRImage.get_data().shape)
-    data[initHRImage.get_data() > args.padding] = 1
-    maskHRImage = nibabel.Nifti1Image(data, initHRImage.affine)
-
-  else:
-    print 'Computing initialization for HR image using the stack of input images'
-    #Create a reference image using the first image 
-    #todo : compute the bounding box containing all input image
-    LRSize    = np.float32(np.array(inputImages[0].header['dim'][1:4]))
-    LRSpacing = np.float32(np.array(inputImages[0].header['pixdim'][1:4]))
-    HRSize    = np.int16(np.ceil(LRSize / HRSpacing * LRSpacing))
-    
-    #Compute the new affine (header) matrix
-    refImageData = np.zeros(HRSize, dtype=np.int16)
-    s = np.eye(4)
-    s[0,0] = inputImages[0].header['pixdim'][1]
-    s[1,1] = inputImages[0].header['pixdim'][2]
-    s[2,2] = inputImages[0].header['pixdim'][3]
-    newAffine = np.dot(inputImages[0].header.get_sform(), np.linalg.inv(s))
-    s[0,0] = HRSpacing[0]
-    s[1,1] = HRSpacing[1]
-    s[2,2] = HRSpacing[2]    
-    newAffine = np.dot(newAffine,s)
-    referenceImage = nibabel.Nifti1Image(refImageData, newAffine)
-    
-    initHRImageData = np.zeros(HRSize, dtype=np.float32)
-    j = 0
-    for i,t in zip(inputImages,inputTransforms):
-      tmpImage = apply_affine_itk_transform_on_image(input_image=i,transform=t[0], center=t[1], reference_image=referenceImage, order=3)
-      nibabel.save(tmpImage,'warped_'+str(j)+'.nii.gz')
-      j = j+1
-      initHRImageData += ( tmpImage.get_data() / np.float32(len(inputImages)) )
-    initHRImage = nibabel.Nifti1Image(initHRImageData, newAffine)
-    nibabel.save(initHRImage,'init.nii.gz')
-    
-    maskHRImageData = np.zeros(HRSize, dtype=np.float32)
-    for i,t in zip(maskImages,inputTransforms):
-      tmpImage = apply_affine_itk_transform_on_image(input_image=i,transform=t[0], center=t[1], reference_image=referenceImage, order=1)
-      maskHRImageData += ( tmpImage.get_data() / np.float32(len(maskImages)) )
-      
-    maskHRImage = nibabel.Nifti1Image(maskHRImageData, newAffine)
-    nibabel.save(maskHRImage,'maskHR.nii.gz')
-
+  ###---- Reading HR image as initialization ----------------------------
+  initHRImage = nibabel.load( args.init )
+  data = np.zeros(initHRImage.get_data().shape)
+  data[initHRImage.get_data() > args.padding] = 1
+  maskHRImage = nibabel.Nifti1Image(data, initHRImage.affine)
         
   #convert 3D images to stacks of 2D slice images
   #create a transform for each 2D slice
@@ -208,7 +167,6 @@ if __name__ == '__main__':
     HRpsf = compute_psf(LRSpacing, HRSpacing, args.psf)
     psfList.append(HRpsf)
   
-
   #Compute H
   HList = []
   for i in range(len(inputImages)):
