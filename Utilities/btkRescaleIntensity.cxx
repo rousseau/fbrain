ade7321506ec9aec08283073775d8a5e0533a681
rousseau <rousseau@wifi-salsa-113.priv.enst-bretagne.fr>
2016-11-25 09:41:52 +0100

    Change short to float (for image type)

--- Utilities/btkRescaleIntensity.cxx
+++ Utilities/btkRescaleIntensity.cxx
@@ -72,7 +72,7 @@ int main(int argc, char *argv[])
     
   
   //ITK declaration
-  typedef short PixelType;
+  typedef float PixelType;
   const   unsigned int        Dimension = 3;
   typedef itk::Image< PixelType, Dimension >    ImageType;
   typedef itk::ImageRegionIterator< ImageType > itkIterator;
